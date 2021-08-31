0424ff651a09c973d673ab6e2bd3e6bfe1e96fc3
tow96 <jose.towe@gmail.com>
2021-08-30 20:10:51 -0500

    Removed the useEffect and moved the closing of the modal only when the deletion fails

--- src/Pages/Wallets/WalletCard.tsx
+++ src/Pages/Wallets/WalletCard.tsx
@@ -10,20 +10,25 @@ import * as HiIcons from 'react-icons/hi';
 
 // Hooks
 import { AuthenticationTokenStore } from '../../Hooks/ContextStore';
+import UseForm from '../../Hooks/UseForm';
 
 // Components
 import Button from '../../Components/Button';
 import Modal from '../../Components/Modal';
+import Input from '../../Components/Input';
 
 // Models
 import { Wallet } from '../../models';
 
+// Utilities
+import CheckNested from '../../Utils/CheckNested';
+
 // Services
 import TransactionService from '../../Services/TransactionService';
-import { useEffect } from 'react';
 
 interface Props extends RouteComponentProps {
   wallet: Wallet;
+  editWallet: (wallet: Wallet) => void;
   deleteWallet: (wallet: Wallet) => void;
 }
 
@@ -37,16 +42,34 @@ const WalletCard = (props: Props): JSX.Element => {
   // Hooks
   const [showEdit, setEdit] = useState(false);
   const [showDelete, setDelete] = useState(false);
+  const [editErrors, setEditErrors] = useState({} as any);
 
-  // This useEffect only exists so it doesn't update when the deleteWallet function is run,
-  // thus avoiding the "state update on an unmounted component" error
-  useEffect(() => {
-    // Empty
-  }, [deleteWallet]);
+  // Edit wallet form
+  const editWalletForm = UseForm(editWallet, {
+    name: props.wallet.name,
+  });
 
   async function editWallet() {
-    console.log(`edit   ${props.wallet._id}`);
-    setEdit(false);
+    try {
+      const editedWallet = editWalletForm.values as Wallet;
+      editedWallet._id = props.wallet._id;
+
+      // Sends the wallet to the API
+      const res = await transactionService.editWallet(editedWallet);
+
+      // Sets the wallet and closes the modal
+      props.editWallet(res.data);
+      setEdit(false);
+    } catch (err) {
+      // If there is a 304 status, then the modal is closed
+      if (err.response.status == 304) setEdit(false);
+      else if (CheckNested(err, 'response', 'data', 'errors')) setEditErrors(err.response.data.errors);
+      else console.log(err.response); // eslint-disable-line no-console
+    }
+  }
+
+  async function resetEditForm() {
+    editWalletForm.values.name = props.wallet.name;
   }
 
   async function deleteWallet() {
@@ -54,14 +77,13 @@ const WalletCard = (props: Props): JSX.Element => {
       await transactionService.deleteWallet(props.wallet._id);
 
       props.deleteWallet(props.wallet);
-    } catch {
-      // console.log(err.response);
+    } catch (err: any) {
+      console.log(err.response); // eslint-disable-line no-console
+      setDelete(false);
     }
-    setDelete(false);
   }
 
   return (
-    // <div onClick={() => props.history.push('/test')}>Test</div>
     <div className="WalletCard">
       <div className="WalletCard__Name" onClick={() => props.history.push(`/home?wallet=${props.wallet._id}`)}>
         {props.wallet.name}
@@ -73,8 +95,35 @@ const WalletCard = (props: Props): JSX.Element => {
         <HiIcons.HiTrash />
       </Button>
       {/* Edit Wallet Modal */}
-      <Modal showModal={showEdit} setModal={setEdit} onAccept={editWallet}>
-        Edit
+      <Modal
+        showModal={showEdit}
+        setModal={setEdit}
+        title="Edit wallet"
+        accept="Save"
+        onAccept={editWallet}
+        onClose={resetEditForm}
+      >
+        <form onSubmit={editWalletForm.onSubmit}>
+          <Input
+            error={editErrors.name ? true : false}
+            label="Name"
+            name="name"
+            type="text"
+            value={editWalletForm.values.name}
+            onChange={editWalletForm.onChange}
+          />
+        </form>
+        <div>
+          {Object.keys(editErrors).length > 0 && (
+            <div className="ui error message">
+              <ul className="list">
+                {Object.values(editErrors).map((value: any) => (
+                  <li key={value}>{value}</li>
+                ))}
+              </ul>
+            </div>
+          )}
+        </div>
       </Modal>
       {/* Delete Wallet Modal */}
       <Modal
